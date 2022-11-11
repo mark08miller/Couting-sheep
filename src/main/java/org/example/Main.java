5729156df5d291a0a978c2b6b4353de1ccf7f29e
Mark MIller <youtte1g@gmail.com>
2022-11-11 19:57:23 +0200

    аfinal commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Boolean[] isEmpty = {true,true,true,true,null};
+        int result = countSheeps(isEmpty);
+        System.out.println(result);
+    }
+
+    public static int countSheeps(Boolean[] arrayOfSheeps) {
+        int counter = 0;
+        for (int i = 0; i < arrayOfSheeps.length; i++) {
+            if (arrayOfSheeps[i] == null) {
+                continue;
+            }
+            if (arrayOfSheeps[i] == true) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }
